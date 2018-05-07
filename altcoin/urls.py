1f2acf08dc404fc68aeaa6a1a1d08eac6bad4a53
Gabriel Díaz <gabrieldiaz31@gmail.com>
2018-05-07 00:36:52 -0300

    Add custom view to get AuthToken with User information

--- altcoin/urls.py
+++ altcoin/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from rest_framework.authtoken import views
+from profiles.views import AuthToken
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^api-token-auth/', views.obtain_auth_token)
+    url(r'^api-token-auth/', AuthToken.as_view())
 ]
